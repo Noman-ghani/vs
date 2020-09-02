ce4a0940542044bfc1e3ac9809558b9e21f29849
noman.ghani <noman.ghani@genetechsolutions.com>
2020-09-02 19:12:36 +0500

    changes db

--- database/migrations/2020_05_22_150407_students.php
+++ database/migrations/2020_05_22_150407_students.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Doctors extends Migration
+class Students extends Migration
 {
     /**
      * Run the migrations.
@@ -13,29 +13,23 @@ class Doctors extends Migration
      */
     public function up()
     {
-        if (!Schema::hasTable('doctors')) {
-            Schema::create('doctors', function (Blueprint $table) {
+        if (!Schema::hasTable('Students')) {
+            Schema::create('Students', function (Blueprint $table) {
                 $table->id();
-                $table->boolean('are_you_doctor');
+                $table->boolean('fees_payed');
                 $table->unsignedInteger('user_id');
                 $table->bigInteger('mobile_no');
                 $table->bigInteger('alternate_contact_no')->nullable();
+                $table->string('cnic', 15);
                 $table->string('province', 100);
                 $table->string('district', 100);
                 $table->string('basic_qualification', 100);
                 $table->string('other_qualification', 100)->nullable();
-                $table->string('workplace_type')->nullable();
-                $table->text('address_government_hospital')->nullable();
-                $table->text('address_private_hospital')->nullable();
-                $table->text('address_personal_hospital')->nullable();
                 $table->string('work_experience')->nullable();
-                $table->string('cnic', 15);
-                $table->string('pmdc_registration_number', 100)->nullable();
-                $table->tinyInteger('has_taken_pretest')->default(0);
+                $table->string('workplace_type')->nullable();
+                $table->text('address_personal_workplace')->nullable();
                 $table->timestamp('pretest_started_at')->nullable();
                 $table->timestamp('pretest_ended_at')->nullable();
-                $table->integer('module_id')->default('1');
-                $table->integer('section_id')->default('1');
                 $table->integer('is_deleted')->default(0);
                 $table->timestamp('deleted_at')->nullable();
                 $table->timestamps();
@@ -50,6 +44,6 @@ class Doctors extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('doctors');
+        Schema::dropIfExists('Students');
     }
 }
